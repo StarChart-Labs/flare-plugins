ed04ccba7cc5a3a9949093d29efd98b5b2324232
Ryan O'Meara <romeara@live.com>
2019-07-23 22:10:01 -0400

    Switch all integration tests to TestGradleProject methodology
    Previously, there were multiple methods for integration-testing plug-ins
    with real project setups - directly referencing files in-place, and
    creating temporary projects. While the first resulted in less file IO,
    the second allows for more complex setups. In the interest of
    standardizing how these tests are written, the second should be applied
    to all test setups.
    
    This change set applies the temporary project method to all integration
    tests not previously using it

--- src/test/java/org/starchartlabs/flare/plugins/test/plugin/DependencyConstraintsPluginIntegrationTest.java
+++ src/test/java/org/starchartlabs/flare/plugins/test/plugin/DependencyConstraintsPluginIntegrationTest.java
@@ -6,13 +6,14 @@
  */
 package org.starchartlabs.flare.plugins.test.plugin;
 
-import java.io.File;
-import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
 import org.gradle.testkit.runner.TaskOutcome;
 import org.starchartlabs.flare.plugins.test.IntegrationTestListener;
+import org.starchartlabs.flare.plugins.test.TestGradleProject;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
@@ -21,21 +22,28 @@ import org.testng.annotations.Test;
 @Listeners(value = { IntegrationTestListener.class })
 public class DependencyConstraintsPluginIntegrationTest {
 
-    private File testProjectDirectoryAll;
+    private static final Path BUILD_FILE_DIRECTORY = Paths.get("org", "starchartlabs", "flare", "plugins", "test",
+            "dependency", "constraints");
+
+    private static final Path BUILD_FILE = BUILD_FILE_DIRECTORY.resolve("build.gradle");
+
+    private static final Path DEPENDENCIES_FILE = BUILD_FILE_DIRECTORY.resolve("dependencies.properties");
+
+    private Path projectPath;
 
     @BeforeClass
     public void setup() throws Exception {
-        URL directory = this.getClass().getClassLoader()
-                .getResource("org/starchartlabs/flare/plugins/test/dependency/constraints");
-
-        testProjectDirectoryAll = new File(directory.toURI());
+        projectPath = TestGradleProject.builder(BUILD_FILE)
+                .addFile(DEPENDENCIES_FILE, Paths.get("dependencies.properties"))
+                .build()
+                .getProjectDirectory();
     }
 
     @Test
     public void applyConstraints() throws Exception {
         BuildResult result = GradleRunner.create()
                 .withPluginClasspath()
-                .withProjectDir(testProjectDirectoryAll)
+                .withProjectDir(projectPath.toFile())
                 .withArguments("tasks", "--info")
                 .withGradleVersion("5.0")
                 .build();
